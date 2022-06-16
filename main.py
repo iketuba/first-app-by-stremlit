3b44100fc41b7c3f028a2350e5029e31266cb9a1
ikegami <masuten0912@gmail.com>
2022-06-16 22:58:29 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Ctrl+/でコメントアウト
+# Ctrl+]でインデントを挿入
+
+import streamlit as st
+import time
+
+st.title('Streamlit超入門')
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.01)
+
+'Done!!!'
+
+left_column, right_column =st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ回答')
